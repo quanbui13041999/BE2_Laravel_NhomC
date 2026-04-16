a8a65e8369d9fbb4f61eb08068a1ac0a516177fc
kaido <leanhtuan02072006@gmail.com>
2026-04-16 12:58:59 +0700

    viet route login

--- NhomC_CrudUser/routes/web.php
+++ NhomC_CrudUser/routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\Controllers_register;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/register', [Controllers_register::class, 'index']);
+Route::post('/register', [Controllers_register::class, 'store']);
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\Controllers_register;
-
-Route::get('/register', [Controllers_register::class, 'index']);
-Route::post('/register', [Controllers_register::class, 'store']);
+// Auth Routes
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
