c91e659d5a4a41b2c82822e23f611518e762ee1c
kaido <leanhtuan02072006@gmail.com>
2026-04-16 12:46:24 +0700

    ham hien thi form login

---
+++ NhomC_CrudUser/app/Http/Controllers/loginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Error;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Psy\ManualUpdater\Checker;
+
+class loginController extends Controller
+{
+    
+}
